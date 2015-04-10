4555fa73f3913d68e7d662199e6c2625f6b5856d
Guilherme Kuhn <g.kuhn0@gmail.com>
2015-04-10 02:48:39 -0300

    100% COVERAGE

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+class SomeBogusClass; end
+
 RSpec.describe ApplicationController, type: :controller do
 
   context "#admin_required!" do
@@ -17,6 +19,20 @@ RSpec.describe ApplicationController, type: :controller do
     it "should not raise AdminRequiredException if current_user is admin" do
       user.admin = true
       expect { controller.admin_required! }.not_to raise_error
+    end
+  end
+
+  context "#not_found" do
+    controller(ApplicationController) do
+      skip_before_filter :authenticate, :account_required, :user_required, only: :index
+      def index
+        raise Mongoid::Errors::DocumentNotFound.new(SomeBogusClass, {})
+      end
+    end
+    it "should render not found with code 404" do
+      get :index
+      expect(response.code).to eq('404')
+      expect(response.body).to eq({:error => 'Not Found'}.to_json)
     end
   end
 
