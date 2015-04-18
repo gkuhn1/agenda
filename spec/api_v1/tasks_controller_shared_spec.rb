de5e7d8e63b96bc53bed6defa6fd3037ced52a30
Guilherme Kuhn <g.kuhn0@gmail.com>
2015-04-18 18:10:51 -0300

    Adicionado tasks calendar specs

---
+++ spec/api_v1/tasks_controller_shared_spec.rb
@@ -0,0 +1,114 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::TasksController, type: :controller do
+
+  let(:account) { FactoryGirl.create(:account) }
+  let(:user) { account.users.first }
+  let(:account2) { FactoryGirl.create(:account, user: user) }
+  let(:account3) { FactoryGirl.create(:account) }
+
+  let(:calendar) { FactoryGirl.create(:calendar, user: user) }
+  let(:task) { FactoryGirl.create(:task, calendar: calendar) }
+
+  before(:each) {
+    @account = account
+    @user = user
+  }
+
+  it_behaves_like "api v1 controller" do
+    let(:extra_params) { {calendar_id: calendar.id} }
+  end
+
+  context "#index" do
+    it_behaves_like "require current_account" do
+      let(:action) {:index}
+      let(:extra_params) { {calendar_id: calendar.id} }
+    end
+
+    it_behaves_like "require current_user" do
+      let(:action) {:index}
+      let(:extra_params) { {calendar_id: calendar.id} }
+    end
+
+  end
+
+  context "#show" do
+    it_behaves_like "require current_account" do
+      let(:action) {:show}
+      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
+    end
+
+    it_behaves_like "require current_user" do
+      let(:action) {:show}
+      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
+    end
+  end
+
+  context "#new" do
+    it_behaves_like "require current_account" do
+      let(:action) {:new}
+      let(:extra_params) { {calendar_id: calendar.id} }
+    end
+
+    it_behaves_like "require current_user" do
+      let(:action) {:new}
+      let(:extra_params) { {calendar_id: calendar.id} }
+    end
+  end
+
+  context "#create" do
+    let(:task_params) { {calendar_id: calendar.id, task: FactoryGirl.attributes_for(:task)} }
+
+    it_behaves_like "require current_account" do
+      let(:action) {:create}
+      let(:extra_params) { task_params }
+    end
+
+    it_behaves_like "require current_user" do
+      let(:action) {:create}
+      let(:extra_params) { task_params }
+    end
+  end
+
+  context "#edit" do
+    it_behaves_like "require current_account" do
+      let(:action) {:edit}
+      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
+    end
+
+    it_behaves_like "require current_user" do
+      let(:action) {:edit}
+      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
+    end
+  end
+
+
+  context "#update" do
+
+    let(:task_params) { {id: task.id, calendar_id: calendar.id, task: FactoryGirl.attributes_for(:task)} }
+
+    it_behaves_like "require current_account" do
+      let(:action) {:update}
+      let(:extra_params) { task_params }
+    end
+
+    it_behaves_like "require current_user" do
+      let(:action) {:update}
+      let(:extra_params) { task_params }
+    end
+  end
+
+  context "#destroy" do
+    it_behaves_like "require current_account" do
+      let(:action) {:destroy}
+      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
+    end
+
+    it_behaves_like "require current_user" do
+      let(:action) {:destroy}
+      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
+    end
+  end
+
+
+end
--- spec/api_v1/tasks_controller_spec.rb
+++ spec/api_v1/tasks_controller_spec.rb
@@ -4,109 +4,50 @@ RSpec.describe Api::V1::TasksController, type: :controller do
 
   let(:account) { FactoryGirl.create(:account) }
   let(:user) { account.users.first }
-  let(:account2) { FactoryGirl.create(:account, user: user) }
-  let(:account3) { FactoryGirl.create(:account) }
+  let(:account2) { FactoryGirl.create(:account) }
+  let(:user2) { FactoryGirl.create(:user) }
 
   let(:calendar) { FactoryGirl.create(:calendar, user: user) }
-  let(:task) { FactoryGirl.create(:task, calendar: calendar) }
-
-  before(:each) {
-    @account = account
-    @user = user
-  }
-
-  it_behaves_like "api v1 controller" do
-    let(:extra_params) { {calendar_id: calendar.id} }
+  let(:calendar2) { FactoryGirl.create(:calendar, user: user2) }
+  let(:calendar_tasks) do
+    [
+      FactoryGirl.create(:task, calendar: calendar),
+      FactoryGirl.create(:task, calendar: calendar),
+      FactoryGirl.create(:task, calendar: calendar),
+      FactoryGirl.create(:task, calendar: calendar),
+    ]
+  end
+  let(:calendar2_tasks) do
+    [
+      FactoryGirl.create(:task, calendar: calendar2),
+      FactoryGirl.create(:task, calendar: calendar2),
+      FactoryGirl.create(:task, calendar: calendar2),
+      FactoryGirl.create(:task, calendar: calendar2),
+    ]
   end
 
+  before(:each) {
+    api_authenticate(user, account)
+    [calendar_tasks, calendar2_tasks]
+  }
+
   context "#index" do
-    it_behaves_like "require current_account" do
-      let(:action) {:index}
-      let(:extra_params) { {calendar_id: calendar.id} }
+    it "should return only current_calendar tasks" do
+      get :index, {calendar_id: calendar.id}
+      expect(assigns(:tasks).size).to eq(4)
+      expect(assigns(:tasks)).to match_array(calendar_tasks)
     end
-
-    it_behaves_like "require current_user" do
-      let(:action) {:index}
-      let(:extra_params) { {calendar_id: calendar.id} }
-    end
-
   end
 
   context "#show" do
-    it_behaves_like "require current_account" do
-      let(:action) {:show}
-      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
+    it "should return only informations about current_calendar tasks" do
+      get :show, {calendar_id: calendar.id, id: calendar_tasks[1].id}
+      expect(response.code).to eq("200")
+      expect(assigns(:task)).to eq(calendar_tasks[1])
     end
-
-    it_behaves_like "require current_user" do
-      let(:action) {:show}
-      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
-    end
-  end
-
-  context "#new" do
-    it_behaves_like "require current_account" do
-      let(:action) {:new}
-      let(:extra_params) { {calendar_id: calendar.id} }
-    end
-
-    it_behaves_like "require current_user" do
-      let(:action) {:new}
-      let(:extra_params) { {calendar_id: calendar.id} }
-    end
-  end
-
-  context "#create" do
-    let(:task_params) { {calendar_id: calendar.id, task: FactoryGirl.attributes_for(:task)} }
-
-    it_behaves_like "require current_account" do
-      let(:action) {:create}
-      let(:extra_params) { task_params }
-    end
-
-    it_behaves_like "require current_user" do
-      let(:action) {:create}
-      let(:extra_params) { task_params }
-    end
-  end
-
-  context "#edit" do
-    it_behaves_like "require current_account" do
-      let(:action) {:edit}
-      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
-    end
-
-    it_behaves_like "require current_user" do
-      let(:action) {:edit}
-      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
-    end
-  end
-
-
-  context "#update" do
-
-    let(:task_params) { {id: task.id, calendar_id: calendar.id, task: FactoryGirl.attributes_for(:task)} }
-
-    it_behaves_like "require current_account" do
-      let(:action) {:update}
-      let(:extra_params) { task_params }
-    end
-
-    it_behaves_like "require current_user" do
-      let(:action) {:update}
-      let(:extra_params) { task_params }
-    end
-  end
-
-  context "#destroy" do
-    it_behaves_like "require current_account" do
-      let(:action) {:destroy}
-      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
-    end
-
-    it_behaves_like "require current_user" do
-      let(:action) {:destroy}
-      let(:extra_params) { {id: task.id, calendar_id: calendar.id} }
+    it "should return 404 if calendar is from other account" do
+      get :show, {calendar_id: calendar.id, id: calendar2_tasks[1].id}
+      expect(response.code).to eq("404")
     end
   end
 
